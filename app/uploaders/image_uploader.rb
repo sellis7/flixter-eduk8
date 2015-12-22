8cc6a625c76071e2c2f3ac114816dddf51bceca0
sellis7 <sellis7@users.noreply.github.com>
2015-12-21 22:47:01 -0500

    File upload resolution established

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,6 +26,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
+  process :resize_to_fill => [800, 350]
   #
   # def scale(width, height)
   #   # do something
